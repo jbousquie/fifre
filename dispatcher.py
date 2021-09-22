43e4533007403b03857a30786571bd71af61e396
Jérôme Bousquié <jerome@bousquie.fr>
2021-09-22 16:53:33 +0200

    more details in classes

--- dispatcher.py
+++ dispatcher.py
@@ -3,19 +3,36 @@
 
 class Dispatcher:
 
-    def enrollSsFifres():
-        return
+    # constructeur
+    def __init__(self):
+        # liste des sous-fifres connus du dispatcher
+        self.ssfifres = []
+
+
+
+    # Gestion des messages depuis et vers les clients
+    # -----------------------------------------------
 
     def parseMessage(msg):
         return
 
     def acknowledge(msg):
         return
-
-    def order():
-        return
     
     def reportClient():
+        return
+
+
+    # Gestion des sous-fifres
+    # -----------------------
+
+
+    # enregistre les sous-fifres connus dans la liste
+    def enrollSsFifres():
+        return
+
+    # ordres donnés au sous-fifre
+    def order():
         return
 
     def stackOrder():
--- message.py
+++ message.py
@@ -1,4 +1,5 @@
-# Classe Message
+# Classe Message  : objet message entre le client et le dispatcher
+import uuid
 
 class Message:
 
@@ -8,3 +9,9 @@ class Message:
         self.date = date
         self.domain = domain
         self.content = content
+        self.msgId = self.generateUniqueId()
+
+    # generate an unique random string
+    def generateUniqueId(self):
+        id = str(uuid.uuid4())
+        return id
